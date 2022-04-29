d2afd8f5d01c022c371bdf2961bf37f351d9c664
Materij <48157664+Materij@users.noreply.github.com>
2022-04-29 16:05:38 +0300

    Update tictactoe02.py

--- tictactoe02.py
+++ tictactoe02.py
@@ -10,52 +10,52 @@ def print_list():
 
 def check():
     if myTTT[1][1] == "X" and myTTT[1][2] == "X" and myTTT[1][3] == "X":
-        print("Победили крестики")
+        print("Crosses won")
         return False
     elif myTTT[2][1] == "X" and myTTT[2][2] == "X" and myTTT[2][3] == "X":
-        print("Победили крестики")
+        print("Crosses won")
         return False
     elif myTTT[3][1] == "X" and myTTT[3][2] == "X" and myTTT[3][3] == "X":
-        print("Победили крестики")
+        print("Crosses won")
         return False
     elif myTTT[1][1] == "X" and myTTT[2][1] == "X" and myTTT[3][1] == "X":
-        print("Победили крестики")
+        print("Crosses won")
         return False
     elif myTTT[1][2] == "X" and myTTT[2][2] == "X" and myTTT[3][2] == "X":
-        print("Победили крестики")
+        print("Crosses won")
         return False
     elif myTTT[1][3] == "X" and myTTT[2][3] == "X" and myTTT[3][3] == "X":
-        print("Победили крестики")
+        print("Crosses won")
         return False
     elif myTTT[1][1] == "X" and myTTT[2][2] == "X" and myTTT[3][3] == "X":
-        print("Победили крестики")
+        print("Crosses won")
         return False
     elif myTTT[1][3] == "X" and myTTT[2][2] == "X" and myTTT[3][1] == "X":
-        print("Победили крестики")
+        print("Crosses won")
         return False
     elif myTTT[1][1] == "0" and myTTT[1][2] == "0" and myTTT[1][3] == "0":
-        print("Победили нолики")
+        print("Zeros won")
         return False
     elif myTTT[2][1] == "0" and myTTT[2][2] == "0" and myTTT[2][3] == "0":
-        print("Победили нолики")
+        print("Zeros won")
         return False
     elif myTTT[3][1] == "0" and myTTT[3][2] == "0" and myTTT[3][3] == "0":
-        print("Победили нолики")
+        print("Zeros won")
         return False
     elif myTTT[1][1] == "0" and myTTT[2][1] == "0" and myTTT[3][1] == "0":
-        print("Победили нолики")
+        print("Zeros won")
         return False
     elif myTTT[1][2] == "0" and myTTT[2][2] == "0" and myTTT[3][2] == "0":
-        print("Победили нолики")
+        print("Zeros won")
         return False
     elif myTTT[1][3] == "0" and myTTT[2][3] == "0" and myTTT[3][3] == "0":
-        print("Победили нолики")
+        print("Zeros won")
         return False
     elif myTTT[1][1] == "0" and myTTT[2][2] == "0" and myTTT[3][3] == "0":
-        print("Победили нолики")
+        print("Zeros won")
         return False
     elif myTTT[1][3] == "0" and myTTT[2][2] == "0" and myTTT[3][1] == "0":
-        print("Победили нолики")
+        print("Zeros won")
         return False
     else:
         return True
@@ -63,14 +63,14 @@ def check():
 print_list()
 
 for j in range(1, 5):
-    a, b = map(int, input("Номер строки через пробел номер столбца для X: ").split())
+    a, b = map(int, input("string number after space column number for X: ").split())
     a = a + 1
     b = b + 1
     myTTT[a][b] = "X"
     print_list()
     if check() is False:
         break
-    c, d = map(int, input("Номер строки через пробел номер столбца для 0: ").split())
+    c, d = map(int, input("string number after space column number for 0: ").split())
     c = c + 1
     d = d + 1
     myTTT[c][d] = "0"
@@ -78,4 +78,4 @@ for j in range(1, 5):
     if check() is False:
         break
 if check():
-    print("Победитель не выявлен")
+    print("Winner not revealed")
